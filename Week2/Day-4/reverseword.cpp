9330962fb781ac8b62368ed7d21fafb8db7a11a9
[Anurag1-star] <anuragpratapr@gmail.com>
2026-06-12 06:23:43 +0530

    Day-4 completed

---
+++ Week2/Day-4/reverseword.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s) {
+    string ans = "";
+    stringstream ss(s);
+    string temp = "";
+    while(ss>>temp){
+        reverse(temp.begin(),temp.end());
+        ans += temp+" ";
+    }
+    return ans.substr(0,ans.size()-1);
+}
+
+int main(){
+    string str = 
+}
