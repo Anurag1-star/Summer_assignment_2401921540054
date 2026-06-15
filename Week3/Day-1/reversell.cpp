fe6625ee8758c593ba9394c1abef9aa66b032af4
[Anurag1-star] <anuragpratapr@gmail.com>
2026-06-15 19:54:48 +0530

    Day-1 completed

---
+++ Week3/Day-1/reversell.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next ==NULL){
+            return head;
+        }
+        ListNode* prev = NULL;
+        ListNode* nex;
+        ListNode* curr = head;
+        while(curr!=NULL){
+            nex = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nex;
+        }
+        return prev;
+    }
+};
