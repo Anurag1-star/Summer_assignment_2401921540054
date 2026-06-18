96a53d49fd3aeb38cb5f44c0910b9c3111a1b2bf
[Anurag1-star] <anuragpratapr@gmail.com>
2026-06-18 05:42:35 +0530

    Day-2 completed

---
+++ Week3/Day-2/Mergetwosortedlist.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* list3 = new ListNode(0);
+        ListNode* current = list3;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val >= list2->val){
+                current->next = new ListNode(list2->val);
+                current =current->next;
+                list2 = list2->next;
+            }
+            else{
+                current->next = new ListNode(list1->val);
+                current =current->next;
+                list1 = list1->next;
+            }
+        }
+        while(list1!=NULL){
+            current->next = new ListNode(list1->val);
+            current =current->next;
+            list1 = list1->next;
+        }
+        while(list2!=NULL){
+            current->next = new ListNode(list2->val);
+            current = current->next;
+            list2  = list2->next;
+        }
+        return list3->next;
+    }
+};
---
+++ Week3/Day-2/palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int>v;
+        bool flag = false;
+        ListNode* temp= head;
+        int i =0 ;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+        temp= head;
+        reverse(v.begin(),v.end());
+        while(temp!=NULL){
+            if(v[i]==temp->val){
+            i++;
+            temp = temp->next;
+            continue;
+                
+            else{
+            return false;
+            
+        }
+    if(temp == NULL){
+        flag = true;
+    }
+    return flag;
+    }
+};
