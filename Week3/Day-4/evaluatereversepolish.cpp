f57318006409235dccc84dcb27a7d5c06e044d6e
[Anurag1-star] <anuragpratapr@gmail.com>
2026-06-20 16:43:39 +0530

    Day-4 completed

---
+++ Week3/Day-4/evaluatereversepolish.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+    int evalRPN(vector<string>& tokens) {
+        int n = tokens.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(tokens[i]=="+"){
+                int second = st.top();
+                st.pop();
+                int first = st.top();
+                st.pop();
+                cout<<first+second<<endl;
+                st.push(first+second);
+            }
+            else if(tokens[i]=="-"){
+                int second = st.top();
+                st.pop();
+                int first = st.top();
+                st.pop();
+                st.push(first-second);
+            }
+             else if(tokens[i]=="*"){
+                int second = st.top();
+                st.pop();
+                int first = st.top();
+                st.pop();
+                st.push(first*second);
+            }
+             else if(tokens[i]=="/"){
+                int second = st.top();
+                st.pop();
+                int first = st.top();
+                st.pop();
+                st.push(first/second);
+            }
+            else{
+                st.push(stoi(tokens[i]));
+            }
+        }
+        return st.top();
+    }
+
+int main(){
+    
+}
